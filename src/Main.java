6f688ccc4d8a90983199b8b85dfa15e671c8160d
frrederikke <frederikke@familienronne.dk>
2020-02-21 08:12:54 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,24 @@
 Write a Java program to convert minutes into a number of years and days
  */
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Write a number of minutes, and i will convert it into years and days");
 
         int minutes = keyboard.nextInt();
-        int years = minutes/525600;
-        int days = minutes/1440;
-        if(years > 0){
-            int restDays = days % 365; // Giver resten af 'days', ud af 365.
-            System.out.println("There are "+years+ " years, and "+restDays+" days on "+minutes+ " minutes");
-        } else{
-            System.out.println("There are "+days+" days on "+minutes+ " minutes");
+        int years = minutes / 525600;
+        int days = minutes / 1440;
+        if (years > 0) {
+            int restDays = days % 365; // Giver resten af 'days', ud af 365. Eks. 9/4=2 , 9%4=1
+            System.out.println("There are " + years + " years, and " + restDays + " days on " + minutes + " minutes");
+        } else {
+            System.out.println("There are " + days + " days on " + minutes + " minutes");
         }
     }
 }
